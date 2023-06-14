467ffd1b5ea3f5bd55bd3204ee7f2e129eb4274a
Vladimir Beliashov <belyash@seznam.cz>
2023-06-14 19:14:53 +0400

    add player

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,27 @@ struct GameState {
     lost: bool,
 }
 
+const PLAYER_LABEL: &'static str = "player";
+
 fn main() {
     let mut game = Game::new();
 
+    init_player(&mut game);
 
+    game.audio_manager.play_music(MusicPreset::WhimsicalPopsicle, 0.2);
 
     game.add_logic(game_logic);
     game.run(GameState {
         health: 5,
         lost: false,
     });
+}
+
+fn init_player(game: &mut Game<GameState>) {
+    let player = game.add_sprite(PLAYER_LABEL, SpritePreset::RacingCarYellow);
+    player.translation.x = -500.0;
+    player.layer = 10.0;
+    player.collision = true;
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
