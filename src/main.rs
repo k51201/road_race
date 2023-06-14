876a7a524c53bee2f9a3086e75ab3ec9bb76142c
Vladimir Beliashov <belyash@seznam.cz>
2023-06-14 19:03:17 +0400

    add state

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use rusty_engine::prelude::*;
 
-struct GameState {}
+struct GameState {
+    health: u8,
+    lost: bool,
+}
 
 fn main() {
     let mut game = Game::new();
 
-    // game setup goes here
+
 
     game.add_logic(game_logic);
-    game.run(GameState {});
+    game.run(GameState {
+        health: 5,
+        lost: false,
+    });
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
