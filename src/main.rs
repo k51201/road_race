4ca700f0f13b8ddf855e70f5a669781b4e4bda1a
Vladimir Beliashov <belyash@seznam.cz>
2023-06-14 20:06:09 +0400

    road movement

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use rusty_engine::prelude::*;
 
 const PLAYER_LABEL: &str = "player";
+const ROADLINE_LABEL: &str = "roadline";
+
 const PLAYER_SPEED: f32 = 250.0;
+const ROAD_SPEED: f32 = 400.0;
 
 struct GameState {
     health: u8,
@@ -14,6 +17,12 @@ fn main() {
     init_player(&mut game);
 
     game.audio_manager.play_music(MusicPreset::WhimsicalPopsicle, 0.2);
+    for i in 0..10 {
+        let label = format!("{}{}", ROADLINE_LABEL, i);
+        let mut line_sprite = game.add_sprite(label, SpritePreset::RacingBarrierWhite);
+        line_sprite.scale = 0.1;
+        line_sprite.translation.x = -600.0 + 150.0 * i as f32;
+    }
 
     game.add_logic(game_logic);
     game.run(GameState {
@@ -51,5 +60,17 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
     if -360.0 >= player_y || player_y >= 360.0 {
         game_state.health = 0;
         println!("left the road");
+    }
+
+    // road movement
+    for sprite in engine.sprites.values_mut() {
+        if sprite.label.starts_with(ROADLINE_LABEL) {
+            let x = &mut sprite.translation.x;
+            if *x <= -675.0 {
+                *x += 1500.0;
+            } else {
+                *x -= ROAD_SPEED * engine.delta_f32;
+            }
+        }
     }
 }
